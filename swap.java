3b7c9eccb08e0a15e7fa1d192f1a2c15e9b54490
Abhijith <abhijithmbhat@gmail.com>
2026-05-04 12:06:28 +0530

    Add swap functionality for two integers
    Implement a swap function using three variables.

---
+++ swap.java
@@ -0,0 +1,15 @@
+#with 3 variable
+  class Main {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        int t = a;
+         a = b;
+         b = t;
+         System.out.println("value of a is " + a+ "value of b "+b);
+    }
+}
+
+
+
+  
